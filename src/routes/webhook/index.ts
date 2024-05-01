c368315b0f51915487f95efac0de1d3f33bd71ce
BrownCarter <lilbrown3000@gmail.com>
2024-05-01 12:31:09 +0100

    head webhook

--- src/routes/webhook/index.ts
+++ src/routes/webhook/index.ts
@@ -2,11 +2,16 @@ import { FastifyPluginAsync } from "fastify";
 
 const webhook: FastifyPluginAsync = async (fastify): Promise<void> => {
   fastify.head("/", (request, reply) => {
-    console.log(request.headers);
+    console.log("circle-head", request.headers);
     return reply.status(200);
   });
 
-  fastify.post("/", { websocket: true }, (socket, req) => {
+  fastify.post("/", (request) => {
+    console.log("circle-body", request.body);
+    return request.body;
+  });
+
+  fastify.get("/", { websocket: true }, (socket, req) => {
     socket.on("message", (_message) => {
       // message.toString() === 'hi from client'
       console.log(req.body);
